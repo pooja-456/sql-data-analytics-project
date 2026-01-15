c28496036598eb43748b1c2790e1171f1d85780f
Pooja M <itsmepooja45@gmail.com>
2026-01-16 01:33:00 +0530

    Create 01_database_exploration.sql

---
+++ scripts/01_database_exploration.sql
@@ -0,0 +1,22 @@
+/*
+===============================================================================
+1) Database Exploration
+===============================================================================
+Purpose:
+    - To explore the structure of the database by identifying available tables.
+    - To inspect column details and metadata for specific tables.
+    - To understand the overall schema before performing analysis.
+
+Tables Used:
+    - INFORMATION_SCHEMA.TABLES
+    - INFORMATION_SCHEMA.COLUMNS
+===============================================================================
+*/
+
+-- Retrieves all tables available in the database to understand the overall schema
+select * from INFORMATION_SCHEMA.tables
+
+
+-- Retrieves column details and metadata for the dim_customers table
+select * from information_schema.columns
+where table_name='dim_customers'
